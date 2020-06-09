ab6640f3f4a7cae61c08f858eef7c8896c8ef072
MarcDuyukun <duyukun1204@163.com>
2020-06-09 16:25:16 +0800

    '移动端配置lib-flexible自适应rem之前'

---
+++ src/containers/App/index.jsx
@@ -0,0 +1,18 @@
+import React ,{ Component } from 'react';
+import './App.scss';
+
+class App extends  Component {
+  render(){
+    return (
+        <div className="App">
+              kkkkk
+              <p className="red">
+                哈哈哈
+              </p>
+
+        </div>
+      )
+  }
+}
+
+export default App;
